8a4db6738eaa48883744a3421a49d37d2aee2a3a
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2023-05-13 07:44:20 -0400

    window useeffect

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -84,7 +84,7 @@ export const HeaderNav: React.FC<headerNavProps> = ({
   menuStatus,
   toggleMenu,
 }) => {
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(window?.innerWidth);
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleResize);
