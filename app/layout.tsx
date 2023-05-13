cff8e87259d147ac697c2f7cd87aad19459680ef
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2023-05-13 08:04:18 -0400

    Window

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,116 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+"use client";
+import "./globals.css";
+import { Fira_Code } from "next/font/google";
+import { useDisclosure, useLocalStorage } from "@mantine/hooks";
+import { HeaderNav } from "@/components/HeaderNav";
+import React, { useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { headerLinks } from "@/lib/copy";
+import Link from "next/link";
 
-export const metadata = {
-  title: 'Vercel Postgres Demo with Prisma',
-  description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
-}
+const FiraCode = Fira_Code({
+  subsets: ["latin"],
+  display: "swap",
+  style: "normal",
+});
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+// export const metadata = {
+//   title: "Boskind Digital",
+//   description: "Boskind Digital LLC",
+// };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
+  const [themeStorage, setTheme] = useLocalStorage({
+    key: "theme",
+    defaultValue: "winter",
+  });
+  const handleThemeChange = () => {
+    setTheme(themeStorage === "winter" ? "dark" : "winter");
+  };
+  const [isMenuOpen, toggleMenu] = useDisclosure(false);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  useEffect(() => {
+    if (width > 650) {
+      toggleMenu.close();
+    }
+  }, [width, toggleMenu]);
+
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
-  )
+    <QueryClientProvider client={queryClient}>
+      <html lang="en" data-theme={themeStorage}>
+        <body className={FiraCode.className}>
+          <HeaderNav
+            theme={themeStorage}
+            toggleTheme={handleThemeChange}
+            menuStatus={isMenuOpen}
+            toggleMenu={toggleMenu.toggle}
+          />
+          <div className="drawer">
+            <input
+              id="my-drawer"
+              type="checkbox"
+              className="drawer-toggle"
+              checked={isMenuOpen}
+              onChange={toggleMenu.toggle}
+            />
+
+            <div className="drawer-content">{children}</div>
+            <div className="drawer-side">
+              <label htmlFor="my-drawer" className="drawer-overlay"></label>
+              <ul className="menu p-4 w-100 bg-base-100 text-base-content">
+                {
+                  <ul className=" p-4 w-80 bg-base-100 ">
+                    {headerLinks.map((link) => {
+                      if (link.subItems) {
+                        return (
+                          <li key={`sidenav${link.label}${link.href}`}>
+                            <div className="group relative flex items-center ">
+                              {link.icon ? <link.icon /> : null}
+                              {link.label}
+                            </div>
+                            <ul className=" p-4 pt-0 w-70 bg-base-100 ">
+                              {link.subItems.map((subItem) => (
+                                <li
+                                  key={`sidenav${subItem.label}${subItem.href}`}
+                                >
+                                  <Link href={subItem.href}>
+                                    {subItem.label}
+                                    {subItem.icon ? <subItem.icon /> : null}
+                                  </Link>
+                                </li>
+                              ))}
+                            </ul>
+                          </li>
+                        );
+                      } else {
+                        return (
+                          <li key={`sidenav${link.label}`}>
+                            <Link href={link.href}>
+                              {link.icon ? <link.icon /> : null}
+                              {link.label}
+                            </Link>
+                          </li>
+                        );
+                      }
+                    })}
+                  </ul>
+                }
+              </ul>
+            </div>
+          </div>
+        </body>
+      </html>
+    </QueryClientProvider>
+  );
 }
---
+++ lib/copy.ts
@@ -0,0 +1,51 @@
+import { linkItemsList } from "@/components/HeaderNav";
+import { CgWebsite } from "react-icons/cg";
+import { BsFillCameraFill } from "react-icons/bs";
+import { FaSchool } from "react-icons/fa";
+import { MdOutlineEngineering } from "react-icons/md";
+
+export const copy = {
+  components: {
+    BoskindDigital: {
+      title: "Boskind Digital",
+      llc: "Boskind Digital LLC",
+      subTitle: "Web Development and Design",
+      photography: "Photography",
+      welcome: "Welcome to Boskind Digital",
+      // large block of copy about web development and photography services
+      copy: `Boskind Digital is a web development and design company based in
+      Derby, Vermont. We specialize in building websites for small businesses
+      and individuals. We also offer photography services for events, portraits,
+      and real estate.`,
+    },
+
+    footer: {
+      contactMe: "Contact Me",
+    },
+  },
+};
+
+export const headerLinks: linkItemsList = [
+  {
+    label: "Web Development",
+    href: "/web-development",
+    icon: CgWebsite,
+    subItems: [
+      {
+        label: "Custom Websites",
+        href: "/web-development",
+        icon: MdOutlineEngineering,
+      },
+      {
+        label: "School Dashboards",
+        href: "/school",
+        icon: FaSchool,
+      },
+    ],
+  },
+  {
+    label: "Photography",
+    href: "/photography",
+    icon: BsFillCameraFill,
+  },
+];
