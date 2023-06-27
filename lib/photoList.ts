675b2b31db69b9cc03c67c545e6d3dcbb58a90f9
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2023-06-27 06:44:55 -0400

    Fix photo URLS?

--- lib/photoList.ts
+++ lib/photoList.ts
@@ -6,25 +6,25 @@ export type Photo = {
 
 export const panoramaList: Photo[] = [
   {
-    url: "https://photos.rboskind.com/api/v1/t/f664c038e3ff19820ee8bb2eb4fffed4dc3812aa/11ofl5mm/fit_3840",
+    url: "https://photos.rboskind.com/api/v1/t/f664c038e3ff19820ee8bb2eb4fffed4dc3812aa/35562ksr/fit_3840",
     title: "Newport Docks",
     description:
       "This is a panorama of the Newport City Docks, with the gateway center and the big tour boat.",
   },
   {
-    url: "https://photos.rboskind.com/api/v1/t/1f15a4cf515462c78bbdd0b7e487dd49d7e48b92/11ofl5mm/fit_3840",
+    url: "https://photos.rboskind.com/api/v1/t/1f15a4cf515462c78bbdd0b7e487dd49d7e48b92/35562ksr/fit_3840",
     title: "Memphremagog Islands",
     description:
       "This is a panorama of lake Memprhemagog showing the islands and the mountains in the background.",
   },
   {
-    url: "https://photos.rboskind.com/api/v1/t/6f82cce58e23db0d26aabd421401708c7075591d/11ofl5mm/fit_3840",
+    url: "https://photos.rboskind.com/api/v1/t/6f82cce58e23db0d26aabd421401708c7075591d/35562ksr/fit_3840",
     title: "Newport City Skyline",
     description:
       "This is a panorama of the Newport City Skyline, from the state building to the marina.",
   },
   {
-    url: "https://photos.rboskind.com/api/v1/t/a1bc4c078d570796f4a4729e2efec5479101ce9c/11ofl5mm/fit_3840",
+    url: "https://photos.rboskind.com/api/v1/t/a1bc4c078d570796f4a4729e2efec5479101ce9c/35562ksr/fit_3840",
     title: "Jay Peak over Lake Memphremagog",
     description: "This is a panorama of Jay Peak over Lake Memphremagog",
   },
