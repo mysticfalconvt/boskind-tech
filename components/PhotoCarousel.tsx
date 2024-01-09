8f134cfea87730f220aaa98d89b17f84813e7934
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2024-01-09 06:47:04 -0500

    Update to react-image

--- components/PhotoCarousel.tsx
+++ components/PhotoCarousel.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Image from "next/image";
-import { Photo, panoramaList } from "@/lib/photoList";
+import { Photo } from "@/lib/photoList";
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+// import stylesheet if you're not already using CSS @import
+import "react-image-gallery/styles/css/image-gallery.css";
 
 type PhotoCarouselProps = {
   photoList: Photo[];
@@ -10,38 +12,14 @@ type PhotoCarouselProps = {
 export default function PhotoCarousel({
   photoList,
   albumName,
-}: PhotoCarouselProps) {
-  return (
-    <div className="carousel w-full" key={albumName}>
-      {photoList.map((photo, index) => {
-        const previousIndex = index === 0 ? photoList.length - 1 : index - 1;
-        const nextIndex = index === photoList.length - 1 ? 0 : index + 1;
-        return (
-          <div
-            id={`slide${index}`}
-            className="carousel-item relative overflow-visible w-full"
-            key={`album-${albumName}-slide${index}`}
-          >
-            <Image
-              src={photo.url}
-              alt={photo.description}
-              className="w-full object-contain "
-              width={0}
-              height={0}
-              priority
-              sizes="100vw"
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href={`#slide${previousIndex}`} className="btn btn-circle">
-                ❮
-              </a>
-              <a href={`#slide${nextIndex}`} className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+}: PhotoCarouselProps): JSX.Element {
+  const images: ReactImageGalleryItem[] = photoList.map((photo) => ({
+    original: photo.url,
+    thumbnail: photo.url,
+    originalAlt: photo.description,
+    // description: photo.description,
+    originalTitle: photo.description,
+  }));
+
+  return <ImageGallery items={images} thumbnailPosition="top" />;
 }
