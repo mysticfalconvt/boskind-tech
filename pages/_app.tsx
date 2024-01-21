ef1b26823ae11bb62be8676be2cf07be598ea3d0
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2024-01-21 14:28:17 -0500

    Add sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import "../styles/globals.css";
 import { Fira_Code } from "next/font/google";
 import { HeaderNav } from "@/components/HeaderNav";
@@ -27,6 +28,28 @@ export const metadata = {
 
 export default function App({ Component, pageProps }: AppProps) {
   const isProduction = process.env.NODE_ENV === "production";
+
+  Sentry.init({
+    dsn: "https://6867b71b74cd8f312d347b351458f54a@o4506610880741376.ingest.sentry.io/4506610882510848",
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: [
+          "localhost",
+          /^https:\/\/yourserver\.io\/api/,
+        ],
+      }),
+      new Sentry.Replay({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
   return (
     <main className={`${FiraCode.className} `}>
       <Head>
